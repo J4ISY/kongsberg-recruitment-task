c39cc38409b0a30e1d36f11d3c3bd8b54abb345a
Jakub Marciniak <kubamar02@gmail.com>
2023-06-14 01:23:06 +0200

    adding dropdown to header and some styling

--- src/components/template/Header.jsx
+++ src/components/template/Header.jsx
@@ -3,27 +3,21 @@ import React, {useState} from "react";
 
 function Header() {
 
-    const [open, setOpen] = useState(false);
-
     return (
         <>
             <header className="header">
                 <div className="header-wrapper">
-                    <Link to='/'><img src="src/assets/images/logo.png" alt="xbox controller" className="header-logo"/></Link>
+                    <Link to='/'><img src="src/assets/images/logo.png" alt="xbox controller"
+                                      className="header-logo"/></Link>
                     <nav className="header-nav">
-                        <Link to='/' className="header-link">Main page</Link>
-                        <span
-                            onClick={() => {setOpen(!open)}}
-                            className="dropdown-btn"
-                        >Browse category</span>
-                        <div className={`dropdown-menu ${open ? 'active' : 'inactive'}`}>
-                            <ul>
-                                <li className='dropdown-items'>
-                                    <Link to='/' className="dropdown-item">Shooting</Link>
-                                    <Link to='/' className="dropdown-item">MMORPG</Link>
-                                    <Link to='/' className="dropdown-item">Fighting</Link>
-                                </li>
-                            </ul>
+                            <Link to="/" className="home-link">Main Page</Link>
+                        <div className="dropdown">
+                            <button className="home-link drop-btn">Dropdown<i className="arrow down"></i></button>
+                            <div className="dropdown-content">
+                                <Link to='' className='dropdown-item'>Element 1</Link>
+                                <Link to='' className='dropdown-item'>Element 2</Link>
+                                <Link to='' className='dropdown-item'>Element 3</Link>
+                            </div>
                         </div>
                     </nav>
                 </div>
